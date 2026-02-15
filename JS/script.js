02cda9bc166b6bb7830f81fcb723951b0c92a13a
Mominulislam1234423 <mominulislamshawon88@gmail.com>
2026-02-15 13:52:33 +0600

    added lodedpet section

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,26 @@
-console.log("hello wolds")
+const loadPetList = () => {
+    fetch("https://openapi.programming-hero.com/api/peddy/categories")
+        .then(res => res.json())
+        .then(data => displayAllPet(data.categories))
+}
+
+const displayAllPet = (categorie) =>{
+    const allPet = document.getElementById("All-pet");
+
+    categorie.forEach(pet => {
+        console.log(pet)
+
+    const button = document.createElement("button");
+    button.classList = "btn bg-[#0E7A8110]";
+    button.innerHTML = `
+    <img class="h-10 w-10" src="${pet.category_icon}">
+    <p class="text-xl font-bold">${pet.category}</p>
+    ` 
+      allPet.append(button)
+    });
+
+  
+}
+
+
+loadPetList();
